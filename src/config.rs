7c3ba0dc682fd776e191a808bd89daa1bf518f2e
Maxim <gestse11@gmail.com>
2023-01-11 01:09:59 +0300

    Добавлен аргумент --key

--- src/config.rs
+++ src/config.rs
@@ -11,7 +11,7 @@ pub struct KeysHandlers {
 pub struct Config {
     pub keys_handler: KeysHandlers,
     pub culture_info_path: String,
-    pub culture_info: Option<HashMap<char, char>>
+    pub culture_info: Option<HashMap<char, char>>,
 }
 
 impl KeysHandlers {
@@ -20,27 +20,38 @@ impl KeysHandlers {
 }
 impl Config {
     pub const CULTURE_INFO_PATH: &'static str = "culture_info.json";
-    
+
     pub fn new(key: Vk, sp_key: Vk) -> Self {
         Self {
             culture_info_path: Self::CULTURE_INFO_PATH.parse().unwrap(),
-            keys_handler: KeysHandlers { key, additional_key: sp_key },
-            culture_info: None
+            keys_handler: KeysHandlers {
+                key,
+                additional_key: sp_key,
+            },
+            culture_info: None,
         }
     }
-    
+
     pub fn setting_config(&mut self) {
-        let args : Vec<String> = args().collect();
-        
+        let args: Vec<String> = args().collect();
+
         if args.len() <= 1 {
             self.culture_info = Some(Self::get_keys(&self.culture_info_path));
-            return; 
+            return;
         }
         let dictionary_args = Self::get_format_dictanary(&args);
         for (key, value) in dictionary_args {
             match key.to_lowercase().as_str() {
-                "--sp-key" => self.keys_handler.additional_key = Self::what_is_special_key(&value).expect("Error on set sp key"),
-                "--culture-file" => self.if_change_path_culture_file(&value).expect("Error change path culture file"),
+                "--sp-key" => {
+                    self.keys_handler.additional_key =
+                        Self::what_is_special_key(&value).unwrap_or(KeysHandlers::ADDITIONAL_KEY)
+                }
+                "--key" => {
+                    self.keys_handler.key = Self::what_is_key(&value).unwrap_or(KeysHandlers::KEY)
+                }
+                "--culture-file" => self
+                    .if_change_path_culture_file(&value)
+                    .expect("Error change path culture file"),
                 _ => (),
             }
         }
@@ -49,38 +60,58 @@ impl Config {
     fn if_change_path_culture_file(&mut self, path: &str) -> Result<(), &'static str> {
         let path_ = Path::new(path);
         path_.try_exists().expect("No such file exits");
-        
-        if !path_.is_file() { return Err("Isn`t file"); }
-        
+
+        if !path_.is_file() {
+            return Err("Isn`t file");
+        }
+
         self.culture_info_path = path.to_owned();
         self.culture_info = Option::from(Self::get_keys(&self.culture_info_path));
         Ok(())
     }
-    
+
+    fn what_is_key(kb_key: &str) -> Result<Vk, &'static str> {
+        let key_with_str = kb_key
+            .to_uppercase()
+            .chars()
+            .next()
+            .expect("Error get first element") as u8;
+        if !(65..=90).contains(&key_with_str) {
+            return Err("char is uncorrected");
+        }
+
+        unsafe { Ok(Vk::from_u8(key_with_str)) }
+    }
+
     fn what_is_special_key(kb_key: &str) -> Result<Vk, &'static str> {
         match kb_key.to_lowercase().as_str() {
-            "alt" => Ok( Vk::Alt ),
+            "alt" => Ok(Vk::Alt),
             "ctrl" | "control" => Ok(Vk::Control),
             "shift" => Ok(Vk::Shift),
             "win" | "windows" => Ok(Vk::RightWin),
             _ => Err("Not have that key"),
         }
     }
-    
+
     fn get_format_dictanary(args: &[String]) -> HashMap<String, String> {
         let mut exit_hash_map: HashMap<String, String> = HashMap::new();
 
         let mut previous_value = &String::new();
         for (index, str) in args.iter().enumerate() {
-            if index == 0 { continue;}
-            if index % 2 != 0  { previous_value = str;}
+            if index == 0 {
+                continue;
+            }
+            if index % 2 != 0 {
+                previous_value = str;
+            }
             exit_hash_map.insert(previous_value.clone().to_owned(), str.clone().to_owned());
         }
 
         exit_hash_map
     }
-    
+
     fn get_keys(culture_info_path: &String) -> HashMap<char, char> {
-        serde_json::from_str(&read_to_string(culture_info_path).expect("Error read culture file")).unwrap()
+        serde_json::from_str(&read_to_string(culture_info_path).expect("Error read culture file"))
+            .unwrap()
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -6,9 +6,9 @@ use clipboard::ClipboardProvider;
 use std::collections::HashMap;
 use std::thread::sleep;
 
-use winput::{message_loop};
 use crate::config::{Config, KeysHandlers};
 use crate::kb_output::KbOutput;
+use winput::message_loop;
 
 const SLP_DURATION: std::time::Duration = std::time::Duration::from_millis(200);
 
@@ -17,36 +17,47 @@ pub fn run() -> Result<(), &'static str> {
         ClipboardProvider::new().expect("Clipboard Context create fail!");
     let mut cfg = Config::new(KeysHandlers::KEY, KeysHandlers::ADDITIONAL_KEY);
     cfg.setting_config();
-    
+
     start_loop(&mut cfg, clipbd_context)
 }
 
-fn start_loop(cfg: &mut config::Config,  mut clipbd_context: ClipboardContext) -> Result<(), &'static str> {
+fn start_loop(cfg: &mut Config, mut clipbd_context: ClipboardContext) -> Result<(), &'static str> {
     let receiver = message_loop::start().unwrap();
     let mut kb = KbOutput::default();
-    
+
     loop {
-        if let message_loop::Event::Keyboard {action: winput::Action::Press, ..} = receiver.next_event() {
+        if let message_loop::Event::Keyboard {
+            action: winput::Action::Press,
+            ..
+        } = receiver.next_event()
+        {
             if cfg.keys_handler.additional_key.is_down() && cfg.keys_handler.key.is_down() {
                 sleep(SLP_DURATION);
                 kb.select_all.launching();
                 sleep(SLP_DURATION);
                 kb.copy.launching();
                 sleep(SLP_DURATION);
-                
-                let string_for_translate = clipbd_context.get_contents().expect("Error get content from clipboard").to_lowercase();
-                let translated_result = translate(string_for_translate, cfg.culture_info.as_mut().unwrap());
-                
-                if let Err(e) = translated_result
-                {
+
+                let string_for_translate = clipbd_context
+                    .get_contents()
+                    .expect("Error get content from clipboard")
+                    .to_lowercase();
+                let translated_result =
+                    translate(string_for_translate, cfg.culture_info.as_mut().unwrap());
+
+                if let Err(e) = translated_result {
                     match e {
-                        "Error get content from clipboard" | "Error set content in clipboard" => continue,
+                        "Error get content from clipboard" | "Error set content in clipboard" => {
+                            continue
+                        }
                         _ => return Err(e),
                     }
-                } else { 
-                    clipbd_context.set_contents( translated_result.unwrap() ).expect("Error set content in clipboard") 
+                } else {
+                    clipbd_context
+                        .set_contents(translated_result.unwrap())
+                        .expect("Error set content in clipboard")
                 }
-                
+
                 sleep(SLP_DURATION);
                 kb.select_all.launching();
                 sleep(SLP_DURATION);
@@ -57,7 +68,10 @@ fn start_loop(cfg: &mut config::Config,  mut clipbd_context: ClipboardContext) -
     }
 }
 
-fn translate(str_for_translate: String, keys: &HashMap<char, char>) -> Result<String, &'static str> {
+fn translate(
+    str_for_translate: String,
+    keys: &HashMap<char, char>,
+) -> Result<String, &'static str> {
     Ok(str_for_translate
         .chars()
         .map(|ch| *keys.get(&ch).unwrap_or(&ch))
